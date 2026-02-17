e6dd298ebe99ef51df07d7d6fabbd69016afe963
Gokul <gokulshyam567@gmail.com>
2026-02-17 15:34:10 -0500

    feat: add RDS module with PostgreSQL support

--- deploy/terraform/modules/rds/outputs.tf
+++ deploy/terraform/modules/rds/outputs.tf
@@ -1,14 +1,39 @@
 output "endpoint" {
   description = "RDS endpoint hostname."
-  value       = local.endpoint
+  value       = aws_db_instance.this.address
 }
 
 output "port" {
   description = "RDS port."
-  value       = local.port
+  value       = aws_db_instance.this.port
 }
 
 output "database_secret_name" {
   description = "Secret name containing DB credentials/URL."
   value       = var.database_secret_name
+}
+
+output "database_secret_arn" {
+  description = "ARN of the AWS-managed master user secret."
+  value       = try(aws_db_instance.this.master_user_secret[0].secret_arn, null)
+}
+
+output "identifier" {
+  description = "RDS instance identifier."
+  value       = aws_db_instance.this.identifier
+}
+
+output "database_name" {
+  description = "Primary database name."
+  value       = aws_db_instance.this.db_name
+}
+
+output "username" {
+  description = "Master username."
+  value       = aws_db_instance.this.username
+}
+
+output "security_group_id" {
+  description = "Database security group ID."
+  value       = aws_security_group.db.id
 }
--- deploy/terraform/modules/rds/variables.tf
+++ deploy/terraform/modules/rds/variables.tf
@@ -27,3 +27,57 @@ variable "database_secret_name" {
   description = "Secret name that will hold DB connection material."
   type        = string
 }
+
+variable "engine_version" {
+  description = "PostgreSQL engine version."
+  type        = string
+  default     = "16.3"
+}
+
+variable "instance_class" {
+  description = "RDS instance class."
+  type        = string
+  default     = "db.t3.micro"
+}
+
+variable "allocated_storage" {
+  description = "Allocated storage in GiB."
+  type        = number
+  default     = 20
+}
+
+variable "max_allocated_storage" {
+  description = "Maximum autoscaled storage in GiB."
+  type        = number
+  default     = 100
+}
+
+variable "db_name" {
+  description = "Initial database name."
+  type        = string
+  default     = "opscopilot"
+}
+
+variable "master_username" {
+  description = "Master username for the PostgreSQL instance."
+  type        = string
+  default     = "opscopilot"
+}
+
+variable "backup_retention_period" {
+  description = "Backup retention period in days."
+  type        = number
+  default     = 7
+}
+
+variable "deletion_protection" {
+  description = "Enable deletion protection on the RDS instance."
+  type        = bool
+  default     = false
+}
+
+variable "skip_final_snapshot" {
+  description = "Skip final snapshot on destroy."
+  type        = bool
+  default     = true
+}
